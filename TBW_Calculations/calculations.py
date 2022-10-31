0ac1f859290b531f8160880ccf294af1b49bdd56
thwasylenko <thwasylenko@gmail.com>
2022-10-31 14:19:40 -0400

    commit

--- TBW_Calculations/calculations.py
+++ TBW_Calculations/calculations.py
@@ -1,60 +1,112 @@
-import math
+import numpy as np
+import csv
 
 def calculate_bmi(height_m, weight):
     bmi = weight / height_m ** 2
     return bmi
 
-def tbw_calc(ro, rinf, height_cm, weight, bmi):
-    re = ro
-    kef = (0.188/bmi) + 0.2883
-    ecw = kef * (((height_cm ** 2) * (math.sqrt(weight)))/re) ** (2/3)
+def tbw_calc_bmi(ro, rinf, height_m, weight, bmi):
+    height_cm = height_m * 100
 
-    ri = ((1/rinf) - (1/ro)) ** (-1)
-    kif = (5.875/bmi) + 0.4194
-    icw = kif * (((height_cm ** 2) * (math.sqrt(weight)))/ri) ** (2/3)
+    re = ro
+    kef = (0.188 / bmi) + 0.2883
+    ecw = kef * ((((height_cm ** 2) * (np.sqrt(weight))) / re) ** (2 / 3))
+
+    ri = np.abs(((1 / rinf) - (1 / ro)) ** (-1))
+    kif = (5.875 / bmi) + 0.4194
+    icw = kif * ((((height_cm ** 2) * (np.sqrt(weight))) / ri) ** (2 / 3))
 
     tbw = ecw + icw
-    tbw_weight = (tbw/weight) * 100
+    tbw_weight = (tbw / weight) * 100
+    return tbw, tbw_weight
+
+def tbw_calc_bis(ro, rinf, height_cm, weight, gender):
+    re = ro
+    ri = np.abs(((1 / rinf) - (1 / ro)) ** (-1))
+    kb = 4.3
+    db = 0.00105
+    pe = 0
+    pi = 0
+    if (gender == "male"):
+        pe = 40.5
+        pi = 273.9
+    else:
+        pe = 39
+        pi = 264.9
+
+    ecw = (1/1000)*((((kb**2)*(pe**2))/db)**(1/3))*((((np.sqrt(weight))*(height_cm**2))/re)**(2/3))
+    icw = (1/1000)*((((kb**2)*(pe**2))/db)**(1/3))*((((np.sqrt(weight))*(height_cm**2))/re)**(2/3))
+
+    tbw = ecw + icw
+    tbw_weight = (tbw / weight) * 100
     return tbw, tbw_weight
 
 
-def collect_info():
-    user_name = input("Name: ").title()
-    height_stft = input("Height: ")
-    weight_stlbs = input("Weight: ")
-    ro_string = input("Ro: ")
-    rinf_string = input("Rinf: ")
+def parse_impedance_file(path_to_file):
 
-    height_ft = float(height_stft)
-    weight_lbs = float(weight_stlbs)
-    ro = float(ro_string)
-    rinf = float(rinf_string)
+    with open(path_to_file) as raw_data:
+        reader = csv.reader(raw_data)
+        row = list(reader)
 
-    height_cm = height_ft * 30.48
-    height_m = height_cm / 100
-    weight_kg = weight_lbs * 0.45359237
+        freq_5k_string = row[5][3]
+        freq_105k_string = row[105][3]
+        freq_250k_string = row[250][3]
 
-    bmi = calculate_bmi(height_m, weight_kg)
-    tbw, tbw_weight = tbw_calc(ro, rinf, height_cm, weight_kg, bmi)
-    inverse_tbw, inverse_tbw_weight = tbw_calc(rinf, ro, height_cm, weight_kg, bmi)
+        freq_5k_int = int(freq_5k_string)
+        freq_105k_int = int(freq_105k_string)
+        #freq_250k_int = int(freq_250k_string)
 
-    print("Total Body Water: " + str(tbw) + " Total Body with weight: " + str(tbw_weight))
-    print("Inverse Total Body Water: " + str(inverse_tbw) + " Inverse Total Body with weight: " + str(inverse_tbw_weight))
+    return freq_5k_int, freq_105k_int, #freq_250k_int
 
-def tester():
-    height_cm = 172
-    height_m = height_cm / 100
-    weight_kg = 82.1
-    ro = 412
-    rinf = 1083
+def main():
+    user_name = input("Name: ").lower()
+    date = input("Date (MM-DD-YY): ")
+    height = input("Height: ")
+    weight = input("Weight: ")
+    test_num = 1
+    user_bmi = calculate_bmi(float(height), float(weight))
+    filepath = "../tbw_data/calc_data/"
+    user_file = user_name + "_" + date + ".txt"
+    #raw_data_filepath = "../tbw_data/raw_data/test_" + test_num + "_" + date
+
+    tbw_list = []
+    tbw_weight_list = []
 
 
+    while test_num < 11:
+        raw_data_filepath = "../tbw_data/raw_data/"+ user_name +"/test_" + str(test_num) + "_" + date + ".csv"
+        freq_5k, freq_105k, freq_250k = parse_impedance_file(raw_data_filepath)
+        tbw, tbw_weight = tbw_calc_bmi(freq_105k, freq_5k,float(height), float(weight), user_bmi)
+        tbw_list.append(tbw)
+        tbw_weight_list.append(tbw_weight)
 
-    bmi = calculate_bmi(height_m, weight_kg)
-    tbw, tbw_weight = tbw_calc(ro, rinf, height_cm, weight_kg, bmi)
-    inverse_tbw, inverse_tbw_weight = tbw_calc(rinf, ro, height_cm, weight_kg, bmi)
+        test_num += 1
 
-    print("Total Body Water: " + str(tbw) + " Total Body with weight: " + str(tbw_weight))
-    print("Inverse Total Body Water: " + str(inverse_tbw) + " Inverse Total Body with weight: " + str(inverse_tbw_weight))
+    sum_tbw = 0
+    sum_tbw_weight = 0
+    for num1 in tbw_list:
+        sum_tbw += num1
+    for num2 in tbw_weight_list:
+        sum_tbw_weight += num2
+    avg_tbw = sum_tbw / 10
+    avg_tbw_weight = sum_tbw_weight / 10
 
-tester()
+    with open(filepath + user_file, "w") as tbw_data:
+        tbw_data.write("name:" + user_name + "\n")
+        tbw_data.write("TBW:\n")
+        counter = 1
+        for num in tbw_list:
+            tbw_data.write("test" + str(counter) + " TBW:" + str(num) + "\n")
+            counter += 1
+        tbw_data.write("Avg:" + str(avg_tbw) + "\n\n")
+        tbw_data.write("TBW with weight:\n")
+        counter2 = 1
+        for num2 in tbw_weight_list:
+            tbw_data.write("test" + str(counter2) + " TBW/Weight:" + str(num2) + "\n")
+            counter2 += 1
+        tbw_data.write("Avg:" + str(avg_tbw_weight) + "\n")
+
+        tbw_data.close()
+
+
+main()
